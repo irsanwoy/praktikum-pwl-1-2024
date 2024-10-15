1d747323ee94ed032224f823cd725a3585b8b640
irsanwoy <irsanwoy54@gmail.com>
2024-10-15 22:39:08 +0700

    setup color palete

--- pertemuan2/tugas-ekorangin/tailwind.config.js
+++ pertemuan2/tugas-ekorangin/tailwind.config.js
@@ -2,8 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        latarGelap: '#1a1b26',    // Background utama
+        panelGelap: '#2e2f45',    // Panel atau kotak input dan daftar
+        biruUtama: '#3a69d3',     // Warna biru utama untuk card atau tombol
+        merahAksen: '#ff4b5c',    // Warna merah untuk tombol hapus
+        abuCheckbox: '#b5b5b5',   // Warna untuk checkbox atau teks selesai
+      },
+    },
   },
   plugins: [],
 }
-
